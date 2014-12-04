6dab8f4fb6627d708084b50d82619544f69bcc13
Grant McConnaughey <mcconnaughey-g@mssu.edu>
2014-12-04 16:10:44 -0600

    Update plugin metadata

--- CacheManagerGrailsPlugin.groovy
+++ CacheManagerGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class CacheManagerGrailsPlugin {
     // the plugin version
     def version = "0.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.3 > *"
+    def grailsVersion = "2.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
@@ -10,10 +10,10 @@ class CacheManagerGrailsPlugin {
 
     // TODO Fill in these fields
     def title = "Cache Manager Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def author = "Grant McConnaughey"
+    def authorEmail = "grantmcconnaughey@gmail.com"
     def description = '''\
-Brief summary/description of the plugin.
+A simple admin tool to manage the various caches in a Grails app.
 '''
 
     // URL to the plugin's documentation
@@ -22,19 +22,13 @@ Brief summary/description of the plugin.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def license = "APACHE"
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "Github", url: "https://github.com/grantmcconnaughey/Grails-Cache-Manager/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/grantmcconnaughey/Grails-Cache-Manager" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
